5cb8c408e75c9ad9772413121ae7fb5de16542bd
hansnow <hansnow2012@gmail.com>
2021-03-31 17:33:57 +0800

    feat(unzip): unzip and save file

--- src/components/download.tsx
+++ src/components/download.tsx
@@ -1,11 +1,15 @@
 import React, { useState, useCallback } from 'react';
+import * as JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 
 const FILE_URL =
   'https://gw.alipayobjects.com/mdn/rms_95cbec/afts/img/A*XZCbTK78DIYAAAAAAAAAAAAAARQnAQ';
 
 export default function Download() {
+  const [file, setFile] = useState(new Uint8Array());
   const [recv, setRecv] = useState(0);
   const [total, setTotal] = useState(0);
+  const [unzipProgress, setUnzipProgress] = useState(0);
   const startDownload = useCallback(async () => {
     const response = await fetch(FILE_URL);
     const reader = response.body?.getReader();
@@ -16,17 +20,30 @@ export default function Download() {
       if (done || !value) {
         break;
       }
+      setFile((oldFile) => new Uint8Array([...oldFile, ...value]));
       setRecv((oldRecv) => oldRecv + value.length);
       // console.log(`Received ${value.length}`);
     }
   }, []);
+  const unzip = useCallback(async () => {
+    const zip = await JSZip.loadAsync(file);
+    zip.forEach((path, f) => {
+      f.async('blob', (info) => {
+        setUnzipProgress(info.percent);
+      }).then((blob) => {
+        saveAs(blob, path);
+      });
+    });
+  }, [file]);
   return (
     <div>
       <button onClick={startDownload}>download</button>
       <div>
         {recv} of {total}
       </div>
-      <div>{((recv * 100) / total).toFixed(2)}%</div>
+      <div>download {((recv * 100) / (total || Infinity)).toFixed(2)}%</div>
+      <button onClick={unzip}>unzip</button>
+      <div>unzip {unzipProgress.toFixed(2)}%</div>
     </div>
   );
 }
